399ae7ce194c5cb56bc0f47f7df00786566bda9d
Beedlebub <beedlebub@gmail.com>
2020-07-28 13:07:51 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,12 +5,9 @@ bluetooth.onBluetoothDisconnected(function () {
     basic.showIcon(IconNames.No)
 })
 bluetooth.startTemperatureService()
-bluetooth.startUartService()
 basic.forever(function () {
     serial.writeNumber(input.temperature())
     serial.writeLine("")
     basic.showNumber(input.temperature())
-    bluetooth.uartWriteNumber(input.temperature())
-    bluetooth.uartWriteLine("")
     basic.pause(2000)
 })
